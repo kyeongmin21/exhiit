4168116e72db242b4d656c5a65491a74d0631608
kim kyeong min <kyeongmin21@naver.com>
2026-05-15 17:26:07 +0900

    domains 대신 remotePatterns로 변경 (최신 방식)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    reactStrictMode: false,
     images: {
-        domains: ['www.culture.go.kr'],
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'www.culture.go.kr',
+                port: '',
+                pathname: '/**', // 모든 경로 허용
+            },
+        ],
+        // 외부 서버 헤더가 max-age=0이어도, Next.js가 최소 하루는 캐싱하게 만듭니다.
+        minimumCacheTTL: 86400,
     },
-  /* config options here */
 };
 
 export default nextConfig;
